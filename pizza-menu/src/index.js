87d50e067e66b35c21a39a07a5d730b0c23c5aee
Jonathan Doucette <jon@digitalforgedesign.com>
2024-06-22 22:13:12 -0400

    finsihed pizza app

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -25,12 +25,19 @@ function Menu() {
   return (
     <div className='menu'>
       <h2>Our Menu</h2>
+
       {pizzaData.length > 0 ? (
-        <ul className='pizzas'>
-          {pizzaData.map(pizza => (
-            <Pizza pizzaObj={pizza} key={pizza.id} />
-          ))}
-        </ul>
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className='pizzas'>
+            {pizzaData.map(pizza => (
+              <Pizza pizzaObj={pizza} key={pizza.id} />
+            ))}
+          </ul>
+        </React.Fragment>
       ) : (
         <p>No Pizzas Available.</p>
       )}
@@ -38,41 +45,68 @@ function Menu() {
   )
 }
 
+// function Menu() {
+//   return (
+//     <div className='menu'>
+//       <h2>Our Menu</h2>
+//       {pizzaData && (
+//         <ul className='pizzas'>
+//           {pizzaData.map(pizza => (
+//             <Pizza pizzaObj={pizza} key={pizza.id} />
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   )
+// }
+
+function Order({ closedHour, openHour }) {
+  return (
+    <div className='order'>
+      <p>
+        We're open from {openHour}:00am until {closedHour}:00pm. Come visit us
+        or order online
+      </p>
+      <button className='btn'>Order Now</button>
+    </div>
+  )
+}
+
 function Footer() {
   const hour = new Date().getHours()
-  const openHour = 12
-  const closedHour = 22
+  const openHour = 1
+  const closedHour = 24
   const isOpen = hour >= openHour && hour <= closedHour
 
-  // if (isOpen) alert("We're currently Open")
-  // else alert("Sorry we're closed")
-  // const mins = new Date().getMinutes()
-  // const seconds = new Date().getSeconds()
+  // if (!isOpen) {
+  //   return (
+  //     <footer className='footer'>
+  //       <p>Sorry, we're closed. We open at {openHour}:00pm</p>
+  //     </footer>
+  //   )
+  // }
 
-  // console.log(`${hour}:${mins}:${seconds}`)
-  // return React.createElement('footer', null, "We're currently open")
   return (
     <footer className='footer'>
       {isOpen ? (
-        <div className='order'>
-          <p>We're open until {closedHour}:00. Come visit us or order online</p>
-          <button className='btn'>Order Now</button>
-        </div>
+        <Order openHour={openHour} closedHour={closedHour} />
       ) : (
-        <p>Sorry, we're closed. We open at {openHour}:00</p>
+        <p>Sorry, we're closed. We open at {openHour}:00pm</p>
       )}
     </footer>
   )
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
+  //if (pizzaObj.soldOut) return null
+
   return (
-    <li className='pizza'>
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
     </li>
   )
