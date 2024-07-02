e0e2e951859c0cd0290f730f66a5823c2b7aca69
Jonathan Doucette <jon@digitalforgedesign.com>
2024-07-01 22:22:54 -0400

    branched out slider with steps

--- steps/src/App2.js
+++ steps/src/App2.js
@@ -8,19 +8,36 @@ function Counter() {
   const [step, setStep] = useState(1)
   const [count, setCount] = useState(0)
 
+  function handleReset() {
+    setStep(1)
+    setCount(0)
+  }
+
   const date = new Date()
   date.setDate(date.getDate() + count)
 
   return (
     <div className='container'>
       <div className='date-steps'>
-        <button onClick={() => setStep(s => s - 1)}>-</button>
+        <input
+          type='range'
+          min='0'
+          max='10'
+          value={step}
+          onChange={e => setStep(Number(e.target.value))}
+        ></input>
+        <span>{step}</span>
+        {/* <button onClick={() => setStep(s => s - 1)}>-</button>
         <span>Step: {step}</span>
-        <button onClick={() => setStep(s => s + 1)}>+</button>
+        <button onClick={() => setStep(s => s + 1)}>+</button> */}
       </div>
       <div className='date-count'>
         <button onClick={() => setCount(c => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          type='text'
+          value={count}
+          onChange={e => setCount(Number(e.target.value))}
+        ></input>
         <button onClick={() => setCount(c => c + step)}>+</button>
       </div>
       <div className='date'>
@@ -33,6 +50,9 @@ function Counter() {
           {date.toDateString()}
         </p>
       </div>
+      {count !== 0 || step !== 1 ? (
+        <button onClick={handleReset}>Reset</button>
+      ) : null}
     </div>
   )
 }
