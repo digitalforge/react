5105e012f85135e2c73a03ac6b884e8f547da6e9
Jonathan Doucette <jon@digitalforgedesign.com>
2024-07-05 17:46:26 -0400

    children

--- steps/src/App.js
+++ steps/src/App.js
@@ -28,25 +28,40 @@ export default function App() {
             <div className={step >= 2 ? 'active' : ''}>2</div>
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
-          <p className='message'>
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
           <div className='buttons'>
-            <button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button textColor='#fff' bgColor='#7950f2' onClick={handlePrevious}>
+              <span>👈 </span>Previous
+            </Button>
+            <Button textColor='#fff' bgColor='#7950f2' onClick={handleNext}>
+              Next <span> 👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  )
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <p className='message'>
+      <h3>
+        Step {step}: {children}
+      </h3>
+    </p>
+  )
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   )
 }
