42c8e41bfaaa3927578f70dc1214e8feec2e534e
Jonathan Doucette <jon@digitalforgedesign.com>
2024-07-13 21:44:48 -0400

    made stars component

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -52,6 +52,7 @@ const average = arr =>
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData)
+  const [watched, setWatched] = useState(tempWatchedData)
 
   return (
     <>
@@ -60,10 +61,13 @@ export default function App() {
         <NumResults movies={movies} />
       </Navbar>
       <Main movies={movies}>
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-        </ListBox>
-        <WatchedBox />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
       </Main>
     </>
   )
@@ -113,15 +117,15 @@ function Main({ children }) {
   return <main className='main'>{children}</main>
 }
 
-function ListBox({ children }) {
-  const [isOpen1, setIsOpen1] = useState(true)
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true)
 
   return (
     <div className='box'>
-      <button className='btn-toggle' onClick={() => setIsOpen1(open => !open)}>
-        {isOpen1 ? '–' : '+'}
+      <button className='btn-toggle' onClick={() => setIsOpen(open => !open)}>
+        {isOpen ? '–' : '+'}
       </button>
-      {isOpen1 && children}
+      {isOpen && children}
     </div>
   )
 }
@@ -148,25 +152,6 @@ function MovieList({ movies }) {
         <Movie movie={movie} />
       ))}
     </ul>
-  )
-}
-
-function WatchedBox() {
-  const [isOpen2, setIsOpen2] = useState(true)
-  const [watched, setWatched] = useState(tempWatchedData)
-
-  return (
-    <div className='box'>
-      <button className='btn-toggle' onClick={() => setIsOpen2(open => !open)}>
-        {isOpen2 ? '–' : '+'}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMovieList watched={watched} />
-        </>
-      )}
-    </div>
   )
 }
 
