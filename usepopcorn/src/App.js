dc0fa1422497d9cc2d7546c86be54ee5ca879560
Jonathan Doucette <jon@digitalforgedesign.com>
2024-08-05 23:18:29 -0400

    Finsihed adding movie detail component

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import StarRating from './components/StarRating'
 
 const tempMovieData = [
   {
@@ -53,12 +54,12 @@ const average = arr =>
 const KEY = 'b83ec059'
 
 export default function App() {
-  const [query, setQuery] = useState('')
-  const [movies, setMovies] = useState([tempMovieData])
-  const [watched, setWatched] = useState(tempWatchedData)
+  const [query, setQuery] = useState('inception')
+  const [movies, setMovies] = useState([])
+  const [watched, setWatched] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState('')
-  const [selectedId, setSelectedId] = useState('tt0096874')
+  const [selectedId, setSelectedId] = useState(null)
 
   // useEffect(function () {
   //   console.log('After initial Render')
@@ -75,7 +76,7 @@ export default function App() {
   // console.log('During Render')
 
   function handleSelectMovie(id) {
-    setSelectedId(selectedId => (id === selectedId ? null : id))
+    setSelectedId(id === selectedId ? null : id)
   }
 
   function handleCloseMovie() {
@@ -240,12 +241,75 @@ function MovieList({ movies, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setIsLoading(true)
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        )
+
+        const data = await res.json()
+        setMovie(data)
+        setIsLoading(false)
+      }
+
+      getMovieDetails()
+    },
+    [selectedId]
+  )
+
   return (
     <div className='details'>
-      <button className='btn-back' onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className='btn-back' onClick={onCloseMovie}>
+              &larr;{' '}
+            </button>
+            <img src={poster} alt={`Poster of the ${title}`} />
+            <div className='details-overview'>
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐️</span>
+                {imdbRating} IMDb rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className='rating'>
+              <StarRating maxRating={10} size={24} />
+            </div>
+
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   )
 }
