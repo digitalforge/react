63e3c14ed8bf2ef62ffc95c046688a248abaa1c2
Jonathan Doucette <jon@digitalforgedesign.com>
2024-08-28 17:41:47 -0400

    added v2 back to master branch

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App from './Appv2'
 // import { useState } from 'react'
 // import StarRating from './components/StarRating'
 
