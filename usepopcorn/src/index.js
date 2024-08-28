3c78607dfdb5393c2e59a25a21b79d40fe62edaf
Jonathan Doucette <jon@digitalforgedesign.com>
2024-08-28 17:39:50 -0400

    custom hooks new

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './Appv2'
+import App from './App'
 // import { useState } from 'react'
 // import StarRating from './components/StarRating'
 
