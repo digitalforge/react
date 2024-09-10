52b1ce55a1c287f770a79c18076e13c6b939c9fc
Jonathan Doucette <jon@digitalforgedesign.com>
2024-09-10 00:51:00 -0400

    more quiz things

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -5,8 +5,10 @@ import Loader from './components/Loader'
 import Error from './components/Error'
 import StartScreen from './components/StartScreen'
 import Question from './components/Question'
+import NextButton from './components/NextButton'
 
 import { useEffect, useReducer } from 'react'
+import Progress from './components/Progress'
 
 const initialState = {
   questions: [],
@@ -25,7 +27,7 @@ function reducer(state, action) {
     case 'dataFailed':
       return { ...state, status: 'error' }
     case 'start':
-      return { ...state, status: 'active' }
+      return { ...state, status: 'active', index: 0, points: 0, answer: null }
     case 'newAnswer':
       const question = state.questions.at(state.index)
       return {
@@ -36,6 +38,11 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       }
+    case 'nextQuestion':
+      if (state.index === state.questions.length - 1) {
+        return { ...state, status: 'finished' }
+      }
+      return { ...state, index: state.index + 1, answer: null }
     default:
       throw new Error('Invalid action type')
   }
@@ -48,6 +55,7 @@ export default function App() {
   )
 
   const numberOfQuestions = questions.length
+  const maxPossiblePoints = questions.reduce((acc, q) => acc + q.points, 0)
 
   useEffect(() => {
     fetch('http://localhost:3001/questions')
@@ -55,6 +63,7 @@ export default function App() {
       .then(data => dispatch({ type: 'dataReceived', payload: data }))
       .catch(err => dispatch({ type: 'dataFailed' }))
   }, [])
+
   return (
     <div className='app'>
       <Header />
@@ -65,12 +74,38 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen questions={numberOfQuestions} dispatch={dispatch} />
         )}
+        {status === 'finished' && (
+          <div>
+            <h2>Quiz Finished</h2>
+            <h3>
+              You scored {points} out of{' '}
+              {questions.reduce((acc, q) => acc + q.points, 0)}
+            </h3>
+            <button
+              className='btn btn-ui'
+              onClick={() => dispatch({ type: 'start' })}
+            >
+              Take Again
+            </button>
+          </div>
+        )}
+
         {status === 'active' && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Progress
+              index={index}
+              numberOfQuestions={numberOfQuestions}
+              maxPoints={maxPossiblePoints}
+              points={points}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
