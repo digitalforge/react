448df461e140f6024743556a51e922177b96bc04
Jonathan Doucette <jon@digitalforgedesign.com>
2024-09-23 16:42:48 -0400

    moved files to component folder

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
-import Home from './pages/Home'
+import Homepage from './pages/Homepage'
 import AppLayout from './pages/AppLayout'
 import PageNotFound from './pages/PageNotFound'
 //import PageNav from './components/PageNav'
@@ -10,10 +11,12 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Homepage />} />
           <Route path='product' element={<Product />} />
           <Route path='pricing' element={<Pricing />} />
           <Route path='app' element={<AppLayout />} />
+
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- world-wise/src/pages/Login.jsx
+++ world-wise/src/pages/Login.jsx
@@ -1,29 +1,32 @@
-import styles from "./Login.module.css";
+import styles from './Login.module.css'
+import { useState } from 'react'
+import PageNav from '../components/PageNav'
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState('jack@example.com')
+  const [password, setPassword] = useState('qwerty')
 
   return (
     <main className={styles.login}>
+      <PageNav />
       <form className={styles.form}>
         <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
+          <label htmlFor='email'>Email address</label>
           <input
-            type="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
+            type='email'
+            id='email'
+            onChange={e => setEmail(e.target.value)}
             value={email}
           />
         </div>
 
         <div className={styles.row}>
-          <label htmlFor="password">Password</label>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
+            type='password'
+            id='password'
+            onChange={e => setPassword(e.target.value)}
             value={password}
           />
         </div>
@@ -33,5 +36,5 @@ export default function Login() {
         </div>
       </form>
     </main>
-  );
+  )
 }
