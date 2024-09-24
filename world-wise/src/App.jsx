954c42b4eb57148cc060d847045ffceaba7c10c0
Jonathan Doucette <jon@digitalforgedesign.com>
2024-09-23 20:50:41 -0400

    added nested routes to ui

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -11,11 +11,16 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
+          <Route index element={<Homepage />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/' element={<Homepage />} />
           <Route path='product' element={<Product />} />
           <Route path='pricing' element={<Pricing />} />
-          <Route path='app' element={<AppLayout />} />
+          <Route path='app' element={<AppLayout />}>
+            <Route index element={<p>List of Cities 1</p>} />
+            <Route path='cities' element={<p>List of Cities</p>} />
+            <Route path='countries' element={<p>List of Countries</p>} />
+            <Route path='form' element={<p>Form</p>} />
+          </Route>
 
           <Route path='*' element={<PageNotFound />} />
         </Routes>
