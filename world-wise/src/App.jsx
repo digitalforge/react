120b22669892e482c1f6a5b419eb05ff97e92498
Jonathan Doucette <jon@digitalforgedesign.com>
2024-09-25 19:45:34 -0400

    finished adding Navigation routes

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import AppLayout from './pages/AppLayout'
 import Login from './pages/Login'
@@ -10,6 +10,7 @@ import CityList from './components/CityList'
 import City from './components/City'
 import PageNotFound from './pages/PageNotFound'
 import CountryList from './components/CountryList'
+import Form from './components/Form'
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -43,17 +44,14 @@ function App() {
           <Route path='product' element={<Product />} />
           <Route path='pricing' element={<Pricing />} />
           <Route path='app' element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to='cities' />} />
             <Route
               path='cities'
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
             <Route path='cities/:id' element={<City />} />
             <Route path='countries' element={<CountryList cities={cities} />} />
-            <Route path='form' element={<p>Form</p>} />
+            <Route path='form' element={<Form />} />
           </Route>
 
           <Route path='*' element={<PageNotFound />} />
